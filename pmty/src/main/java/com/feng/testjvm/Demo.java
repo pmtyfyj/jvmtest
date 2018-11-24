3957de7faf02a33e5ba9f6a77dc467ce3f540742
fengyongjie <18205062051@163.com>
2018-11-24 16:09:07 +0800

    Java虚拟机测试代码

---
+++ pmty/src/main/java/com/feng/testjvm/Demo.java
@@ -0,0 +1,8 @@
+package com.feng.testjvm;
+
+/**
+ * @author: PMTY
+ * @create: 2018-11-24 16:06
+ **/
+public class Demo {
+}
---
+++ pmty/src/main/java/com/feng/testjvm/testgc/ReferenceCountingGC.java
@@ -0,0 +1,9 @@
+package com.feng.testjvm.testgc;
+
+/**
+ * 测试引用计数算法的缺陷
+ * @author: PMTY
+ * @create: 2018-11-24 16:07
+ **/
+public class ReferenceCountingGC {
+}
