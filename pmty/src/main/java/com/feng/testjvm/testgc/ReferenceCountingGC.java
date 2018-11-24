b1b7a5e1d1809ad4a4e19c65dac531ade6578991
fengyongjie <18205062051@163.com>
2018-11-24 16:28:32 +0800

    GC测试代码

--- pmty/src/main/java/com/feng/testjvm/testgc/ReferenceCountingGC.java
+++ pmty/src/main/java/com/feng/testjvm/testgc/ReferenceCountingGC.java
@@ -6,4 +6,19 @@ package com.feng.testjvm.testgc;
  * @create: 2018-11-24 16:07
  **/
 public class ReferenceCountingGC {
+
+    public Object instance = null;
+    private static final int count = 1024 * 1024;
+    private byte[] bigSize = new byte[2 * count];
+
+    public static void main(String[] args) {
+        ReferenceCountingGC gcA = new ReferenceCountingGC();
+        ReferenceCountingGC gcB = new ReferenceCountingGC();
+        gcA.instance = gcB;
+        gcB.instance = gcA;
+        gcA = null;
+        gcB = null;
+
+        System.gc();
+    }
 }
